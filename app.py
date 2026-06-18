daa446459cb3460808413662265247302d23dc1c
SystemHub00 <systemifp0909@gmail.com>
2026-06-18 16:15:54 -0300

    FORTALEZA

--- app.py
+++ app.py
@@ -786,7 +786,7 @@ TEMPLATE_WIZARD = """\
                                 <div class="hero-highlight">
                                     <strong>CURSOS DISPONÍVEIS:</strong>
                                     <div class="cursos-lista">
-                                        <span class="curso-tag">&#128218; 26/TRNC 02 - Trancista</span>
+                                        <span class="curso-tag">&#128218; 26/TRNC 01 - Trancista</span>
                                         <span class="curso-tag">&#128218; 26/OFDN 05 - Oficina de Dança (São João)</span>
                                     </div>
                                 </div>
